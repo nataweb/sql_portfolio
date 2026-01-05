c145381471e4dd938149484bf2c35c5ec85d6b13
Nata Web <88946640+nataweb@users.noreply.github.com>
2026-01-06 00:22:27 +0300

    Add files via upload
    This file demonstrates the use of SQL CASE statements to apply conditional logic in queries.
    Examples include creating derived columns, handling NULL values, categorizing data, and applying business rules for analytical use cases.

---
+++ Case Statements.sql
@@ -0,0 +1,60 @@
+---------------- CASE Statements ------------------
+/*Generate a report showing the total sales for each category:
+-High: if the sakes higher than 50
+-Medium: If the sales between 20 and 50
+-Low: If sales equal or lower than 20
+Sort the result from lowest to highest*/
+SELECT 
+	Category,
+	SUM(Sales) as TotalSales
+FROM(
+	SELECT 
+	OrderId,Sales,
+	CASE 
+		WHEN Sales > 50 THEN 'Highh'
+		WHEN Sales > 20 THEN 'Medium'
+		ELSE 'Low'
+		END Category 
+	FROM Sales.Orders)t 
+GROUP BY Category
+ORDER BY TotalSales DESC
+
+-------------- Task #2 --------------------
+/*	Retrieve employee details 
+with gender displayed as full text	*/
+SELECT *,
+CASE 
+	WHEN Gender = 'M' THEN 'Male'
+	WHEN Gender = 'F' THEN 'Female'
+	ELSE 'Not Available'
+END Full_Gender
+FROM Sales.Employees
+-------------------  Task #3 ---------------
+/*	Retrieve customer  details 
+with abbreviated country code	*/
+
+SELECT * ,
+CASE 
+	WHEN Country = 'USA' THEN 'US'
+	WHEN Country = 'Germany' THEN 'DE'
+	ELSE 'Unknown'
+END CountryCode
+FROM Sales.Customers
+------------------ Task #4 -------------------
+/* Find the average scores of customers and treat Null as 0.
+And additional provide details such CustomerId & LastName */
+SELECT 
+	CustomerId,
+	LastName,
+	Score,
+CASE 
+	WHEN Score IS NULL THEN 0
+	ELSE Score
+END ScoreClean,
+AVG(CASE 
+	WHEN Score IS NULL THEN 0
+	ELSE Score
+END) OVER() AS AvgCustomerClean,
+AVG(Score)  OVER() AS AvgCustomer
+FROM Sales.Customers
+------------------------------------------------------
